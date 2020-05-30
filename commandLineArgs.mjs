91e6d4372a9c6f22d5168567cb4e0cdfbce8b391
Taisuke Fukuno <fukuno@jig.jp>
2020-05-31 05:42:39 +0900

    fix defaultOption

--- commandLineArgs.mjs
+++ commandLineArgs.mjs
@@ -7,14 +7,23 @@ const commandLineArgs = function (opt) {
     }
     for (let i = 0; i < Deno.args.length; i++) {
       const arg = Deno.args[i];
-      if (arg === "--" + o.name || arg === "-" + o.alias || o.defaultOption) {
-        if (o.type === Boolean) {
-          options[o.name] = true;
-        } else if (o.type === String) {
-          options[o.name] = Deno.args[i + 1];
-          i++;
+      if (arg.startsWith("-")) {
+        if (arg === "--" + o.name || arg === "-" + o.alias) {
+          if (o.type === Boolean) {
+            options[o.name] = true;
+          } else if (o.type === String) {
+            options[o.name] = Deno.args[i + 1];
+            i++;
+          } else if (o.type === Number) {
+            options[o.name] = parseInt(Deno.args[i + 1]);
+            i++;
+          }
+        }
+      } else if (o.defaultOption) {
+        if (o.type === String) {
+          options[o.name] = arg;
         } else if (o.type === Number) {
-          options[o.name] = parseInt(Deno.args[i + 1]);
+          options[o.name] = parseInt(arg);
         }
       }
     }

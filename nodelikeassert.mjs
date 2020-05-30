a0dff49058a239b226c64fb49da2a55c20e66ee9
Taisuke Fukuno <fukuno@jig.jp>
2020-05-30 21:57:03 +0900

    非同期test対応

--- nodelikeassert.mjs
+++ nodelikeassert.mjs
@@ -11,17 +11,37 @@ const assert = {
   deepEqual: assertStrictEq,
 };
 
-const it = Deno.test;
+function isAsync(func) {
+  return func.constructor.name === "AsyncFunction";
+}
+
+let asyncflg = false;
+
+const it = (name, func) => {
+  asyncflg = isAsync(func);
+  Deno.test(name, func);
+}
 
 const makeDirname = importmetaurl => importmetaurl.substring(0, importmetaurl.lastIndexOf('/')).substring("file://".length)
 
 // const __dirname = import.meta.url.substring(0, import.meta.url.lastIndexOf('/')).substring("file://".length)
 
+let beforefunc = null;
+const before = f => beforefunc = f;
+let afterfunc = null;
+const after = f => afterfunc = f;
+
 const expect = test => {
   const res = {
     deep: {
       equal: chk => {
-        denoassert(test, chk);
+        if (!asyncflg) {
+          denoassert(test, chk);
+        } else {
+          if (!Object.is(test. chk)) {
+            throw new Error(test + " is not " + chk);
+          }
+        }
         return res;
       }
     },
@@ -35,7 +55,13 @@ const expect = test => {
         denoassert(true);
       },
       equal: chk => {
-        denoassert(test, chk);
+        if (!asyncflg) {
+          denoassert(test, chk);
+        } else {
+          if (test !== chk) { // !Object.is(test. chk)) {
+            throw new Error(test + " != " + chk);
+          }
+        }
         return res;
       },
       have: {
@@ -61,4 +87,4 @@ const expect = test => {
   return res;
 }
 
-export { describe, assert, it, expect, makeDirname };
+export { describe, assert, it, expect, before, after, makeDirname };

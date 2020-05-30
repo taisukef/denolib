cba42fbd12344d61d6d3ad89c2f4028d8fb03a33
Taisuke Fukuno <fukuno@jig.jp>
2020-05-30 09:50:43 +0900

    add nodelikeassert

---
+++ nodelikeassert.mjs
@@ -0,0 +1,16 @@
+import { assert as denoassert, assertStrictEq } from "https://deno.land/std/testing/asserts.ts";
+
+const describe = (name, testfunc) => {
+  console.log('test start: ', name);
+  testfunc();
+}
+
+const assert = {
+  strictEqual: assertStrictEq,
+  ok: denoassert,
+  deepEqual: assertStrictEq,
+};
+
+const it = Deno.test;
+
+export { describe, assert, it };
